8a32f76914b54c685884cdb9d744b86a7d92ac49
Sudha Susarita <psxssb@nottingham.ac.uk>
2019-08-02 03:06:55 +0100

    Initial commit after base is setup
    AR , unity (speech and text integrated

---
+++ Watson-Unity-ARKit-master/Assets/Scripts/LoaderScript.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoaderScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    GameObject panel;
+    
+    void Start()
+    {
+        panel = GameObject.Find("Panel");
+        panel.gameObject.SetActive(true);
+      
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void voiceInput()
+    {
+        Debug.Log("plays");
+        SceneManager.LoadScene("Main");
+    }
+    public void textInput() {
+        Debug.Log("plays");
+        // Application.OpenURL("https://assistant-chat-eu-gb.watsonplatform.net/web/public/e91fd145-6292-47ff-b10c-b4f2af473345");
+        chatApplication();
+    }
+
+    private void chatApplication()
+    {
+
+    }
+}
